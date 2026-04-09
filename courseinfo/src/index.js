4b1a94df1c794d7b3dca2061c0e3319329611316
ericbojado <eric.bojado.flo@gamil.com>
2026-04-08 22:37:08 -0600

    Modificacion de index.js

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';  
+/*
+const Title = (props) => {
+  return <h1>{props.course}</h1>
+}
+Todo lo Anterior se puede simplificar en lo siguiente:*/
+const Title = ({encabezado}) => <h1>{encabezado}</h1>
+
+const Content = (props) => {
+  return(
+    <div>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const suma = props.exercises1 + props.exercises2 + props.exercises3
+
+  return (<p>Number of exercises {suma}</p>)
+}
+
+const App = () => {
+  const texto = 'Desarrollo de aplicaciones Half Stack'
+  const part1 = 'Fundamentos de React'
+  const exercises1 = 10
+  const part2 = 'Utilizar props para pasar datos'
+  const exercises2 = 7
+  const part3 = 'Estado de un componente'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Title encabezado={texto} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
